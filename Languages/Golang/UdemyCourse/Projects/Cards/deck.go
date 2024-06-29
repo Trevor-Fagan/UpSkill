8de4d4dfe3684f1f959a92bdba900bf53afd6ad3
Trevor Fagan <trevorfagan77@gmail.com>
2024-06-28 22:08:14 -0500

    Added deck creation functionality

--- Languages/Golang/UdemyCourse/Projects/Cards/deck.go
+++ Languages/Golang/UdemyCourse/Projects/Cards/deck.go
@@ -5,8 +5,23 @@ import "fmt"
 // Create a new type of deck which is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			newCard := value + " of " + suit
+			cards = append(cards, newCard)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
-	for index, card := range d {
-		fmt.Println(index, card)
+	for _, card := range d {
+		fmt.Println(card)
 	}
 }
