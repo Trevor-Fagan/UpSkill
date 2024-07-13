9e639caef9db491c2ad02f3cca32be512a8708f2
Trevor Fagan <trevorfagan77@gmail.com>
2024-07-13 14:19:11 -0500

    Added bootstrap styling to project

--- Languages/Golang/GoWebAppsCourse/FirstApp/main.go
+++ Languages/Golang/GoWebAppsCourse/FirstApp/main.go
@@ -3,30 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"text/template"
 )
 
 const portNumber = ":3000"
-
-// Home page handler
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.html")
-}
-
-// About page handler
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.html")
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-
-	if err != nil {
-		fmt.Println("error parsing tempalte: ", err)
-		return
-	}
-}
 
 func main(){
 	http.HandleFunc("/", Home)
