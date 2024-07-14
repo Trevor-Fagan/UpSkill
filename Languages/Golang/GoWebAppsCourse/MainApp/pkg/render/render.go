46215b7fd72707f1665a024ab349a109b6ec254f
Trevor Fagan <trevorfagan77@gmail.com>
2024-07-14 11:47:07 -0500

    Added templating to pages

--- Languages/Golang/GoWebAppsCourse/MainApp/pkg/render/render.go
+++ Languages/Golang/GoWebAppsCourse/MainApp/pkg/render/render.go
@@ -4,12 +4,18 @@ import (
 	"text/template"
 	"fmt"
 	"net/http"
+	"os"
 )
 
 // Render HTML templates
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("../../templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
+	parsedTemplate, err := template.ParseFiles("templates/" + tmpl, "templates/base.layout.tmpl")
+	if err != nil {
+		fmt.Println("error caught: ", err)
+		os.Exit(1)
+	}
+
+	err = parsedTemplate.Execute(w, nil)
 
 	if err != nil {
 		fmt.Println("error parsing tempalte: ", err)
