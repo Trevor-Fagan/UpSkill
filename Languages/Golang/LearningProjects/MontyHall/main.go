e7b1ce96f3f3cf46a28caa55954e1db7d9526370
Trevor Fagan <trevorfagan77@gmail.com>
2024-10-26 10:23:47 -0500

    Updates to monty hall

--- Languages/Golang/LearningProjects/MontyHall/main.go
+++ Languages/Golang/LearningProjects/MontyHall/main.go
@@ -14,18 +14,44 @@ Requirements:
 func main() {
 	for i := 0; i < 5; i++ {
 		numSimulations := math.Pow(10, float64(i))
-		doors := 3
-		correctGuesses := 0
+		//correctGuessesStay := userStaySimulation(int(numSimulations))
+		correctGuessesSwitch := userSwtichSimulation(int(numSimulations))
 
-		for j := 0; j < int(numSimulations); j++ {
-			computerGuess := 1 + rand.Intn(doors)
-			userGuess := 1 + rand.Intn(doors)
+		fmt.Printf("%d: %f\n", int(numSimulations), 100*float64(correctGuessesSwitch)/numSimulations)
+	}
+}
 
-			if userGuess == computerGuess {
+func userStaySimulation(numSimulations int) int {
+	doors := 3
+	correctGuesses := 0
+
+	for i := 0; i < numSimulations; i++ {
+		computerGuess := 1 + rand.Intn(doors)
+		userGuess := 1 + rand.Intn(doors)
+
+		if userGuess == computerGuess {
+			correctGuesses++
+		}
+	}
+
+	return correctGuesses
+}
+
+func userSwtichSimulation(numSimulations int) int {
+	correctGuesses := 0
+
+	for i := 0; i < numSimulations; i++ {
+		computerGuess := 1 + rand.Intn(3)
+		userGuess := 1 + rand.Intn(3)
+
+		if computerGuess != userGuess {
+			genNumber := rand.Intn(2)
+
+			if genNumber == 0 {
 				correctGuesses++
 			}
 		}
-
-		fmt.Printf("%d: %f\n", int(numSimulations), 100*float64(correctGuesses)/numSimulations)
 	}
+
+	return correctGuesses
 }
