664f9301ae6b8fb613dc156899f1660d01810833
Trevor Fagan <trevorfagan77@gmail.com>
2024-10-26 11:10:23 -0500

    Simple weather checker

---
+++ Languages/Golang/LearningProjects/Weather/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"github.com/joho/godotenv"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("Location: ")
+		location, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		location = strings.TrimSpace(location)
+
+		queryString := "http://api.weatherstack.com/current?access_key=" + os.Getenv("API_KEY") + "&query=" + location + "&units=f"
+		weatherResp := getWeather(queryString)
+
+		var result map[string]interface{}
+		_ = json.Unmarshal([]byte(weatherResp), &result)
+
+		if resultLocation, ok := result["location"].(map[string]interface{}); ok {
+			fmt.Println(resultLocation["name"])
+		}
+
+		if current, ok := result["current"].(map[string]interface{}); ok {
+			fmt.Printf("Temperature: %f F\n", current["temperature"])
+		}
+	}
+}
+
+func getWeather(queryString string) []byte {
+	resp, err := http.Get(queryString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return bodyBytes
+}
